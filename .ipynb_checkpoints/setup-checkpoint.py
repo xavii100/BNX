b519b84b369f0a31652584cb7e269bfa8ec6384a
xavii100 <jav_08@hotmail.com>
2021-06-23 20:16:15 -0500

    Actualizacion Dockfile

---
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,8 @@
+from app.listener import Listener
+from app import create_app
+
+if __name__ == "__main__":
+    create_app()
+    listener = Listener()
+    listener.check_outbox()
+    #listener.check_file_inbox()
---
+++ app/.ipynb_checkpoints/listener-checkpoint.py
@@ -0,0 +1,39 @@
+from app.service_impl import ServiceImpl
+from app.properties import Properties
+from app.log import log
+from app import utils
+
+import os
+import time
+
+service = ServiceImpl()
+prop = Properties()
+
+class Listener():
+
+    def check_outbox(self):    
+        try:
+            path_dir_names = prop.INBOX_PATH
+            path_inbox = utils.get_path_from_list(*path_dir_names)
+            tmp_path_dir_names = prop.TEMP_PATH
+            path_tmp = utils.get_path_from_list(*tmp_path_dir_names)
+            log.info('Starting listener inbox on %s', path_inbox)
+            while True:
+                utils.touch(path_tmp)
+                self.start_listener(path_inbox)
+                time.sleep(prop.TIME_WAITED)
+                utils.del_touch(path_tmp)
+        except KeyboardInterrupt:
+            self.observer.stop()
+
+    def start_listener(self, path_inbox):
+        directories = os.listdir(path_inbox)
+        if len(directories) != 0:
+            log.info('Files were found in the directory %s', directories)
+            self.send_files_to_service(directories, path_inbox)
+    
+    def send_files_to_service(self, directories, path_inbox):
+        for file_name in directories:
+            service.send_file(file_name)
+            # time between files
+            time.sleep(2)
